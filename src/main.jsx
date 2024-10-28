44e56c5782e8f7629cadf279029600e0b60ee6cb
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2024-10-28 11:41:45 +0100

    update star component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,33 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import StarRating from "./StarRating";
-import App from "./App.jsx";
-import "./index.css";
+// import App from "./App.jsx";
+// import "./index.css";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onMovieRate={setMovieRating} />
+      <p>These movie is rated {movieRating} </p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    {/* <StarRating maxRating={5} /> */}
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={5}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
+    <Test />
   </StrictMode>
 );
